23af28113f5faae9cfef968635dc5bb8e21c104c
archduek <124202529+archduek@users.noreply.github.com>
2023-05-23 21:33:13 -0400

    Update

--- index.js
+++ index.js
@@ -36,17 +36,67 @@ playerForm.addEventListener('submit', function(event){
 const firstBaseNameInput = document.getElementById("first-base");
 
 firstBaseNameInput.addEventListener("input", function(){
+    //Store input values as they are typed
+
     const firstBaseName = firstBaseNameInput.value;
     console.log(firstBaseName);
-})
+});
 
 const secondBaseNameInput = document.getElementById("second-base");
 
 secondBaseNameInput.addEventListener("input", function(){
     const secondBaseName = secondBaseNameInput.value;
     console.log(secondBaseName);
-})
+});
 
+const thirdBaseNameInput = document.getElementById("third-base");
+
+thirdBaseNameInput.addEventListener("input", function(){
+    const thirdBaseName = thirdBaseNameInput.value;
+    console.log(thirdBaseName);
+});
+
+const shortstopNameInput = document.getElementById("shortstop");
+
+shortstopNameInput.addEventListener("input", function(){
+    const shortstopName = shortstopNameInput.value;
+    console.log(shortstopName);
+});
+
+const catcherNameInput = document.getElementById("catcher");
+
+catcher.addEventListener("input", function(){
+    const catcherName = catcherNameInput.value;
+    console.log(catcherName);
+});
+
+const leftFieldNameInput = document.getElementById("left-field");
+
+leftFieldNameInput.addEventListener("input", function(){
+    const leftFieldName = leftFieldNameInput.value;
+    console.log(leftFieldName);
+});
+
+const rightFieldNameInput = document.getElementById("right-field");
+
+rightFieldNameInput.addEventListener("input", function(){
+    const rightFieldName = rightFieldNameInput.value;
+    console.log(rightFieldName);
+});
+
+const centerFieldNameInput = document.getElementById("center-field");
+
+centerFieldNameInput.addEventListener("input", function(){
+    const centerFieldName = centerFieldNameInput.value;
+    console.log(centerFieldName);
+});
+
+const designatedHitterNameInput = document.getElementById("designated-hitter");
+
+designatedHitterNameInput.addEventListener("input", function(){
+    const designatedHitterName = designatedHitterNameInput.value;
+    console.log(designatedHitterName);
+});
 
 
 
